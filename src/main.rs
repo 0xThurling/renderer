bb5c9e982b828ff0ad8863fc0fac23383318b911
Jacques Thurling <jthurling78@gmail.com>
2025-08-10 17:29:58 +0200

    Added the fullscreen mode

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,16 @@ use constants::*;
 use sdl2_sys::SDL_CreateTexture;
 use sdl2_sys::SDL_DestroyRenderer;
 use sdl2_sys::SDL_DestroyWindow;
+use sdl2_sys::SDL_DisplayMode;
 use sdl2_sys::SDL_Event;
+use sdl2_sys::SDL_GetCurrentDisplayMode;
 use sdl2_sys::SDL_PollEvent;
 use sdl2_sys::SDL_Quit;
 use sdl2_sys::SDL_RenderClear;
 use sdl2_sys::SDL_RenderCopy;
 use sdl2_sys::SDL_RenderPresent;
 use sdl2_sys::SDL_SetRenderDrawColor;
+use sdl2_sys::SDL_SetWindowFullscreen;
 use sdl2_sys::SDL_Texture;
 use sdl2_sys::SDL_UpdateTexture;
 use sdl2_sys::{
@@ -26,8 +29,8 @@ use sdl2_sys::{
 
 extern crate sdl2_sys;
 
-static WINDOW_WIDTH: i32 = 800;
-static WINDOW_HEIGHT: i32 = 600;
+static mut WINDOW_WIDTH: i32 = 800;
+static mut WINDOW_HEIGHT: i32 = 600;
 
 // unsafe code global variables
 static mut SDL_WINDOW: *mut SDL_Window = std::ptr::null_mut();
@@ -47,6 +50,19 @@ fn initialise_window() -> bool {
             "SDL init failed: {:?}",
             std::ffi::CStr::from_ptr(SDL_GetError())
         );
+
+        // Use SDL to query what is the max screen width and height
+        let mut display_mode: SDL_DisplayMode = std::mem::zeroed();
+        let result = SDL_GetCurrentDisplayMode(0, &mut display_mode);
+
+        assert!(
+            result == 0,
+            "SDL_GetCurrentDisplayMode failed: {:?}",
+            std::ffi::CStr::from_ptr(SDL_GetError())
+        );
+
+        WINDOW_WIDTH = display_mode.w;
+        WINDOW_HEIGHT = display_mode.h;
 
         // Create SDL WINDOW
         SDL_WINDOW = SDL_CreateWindow(
@@ -75,6 +91,14 @@ fn initialise_window() -> bool {
             "SDL_CreateRenderer failed: {:?}",
             std::ffi::CStr::from_ptr(SDL_GetError())
         );
+
+        let result = SDL_SetWindowFullscreen(SDL_WINDOW, SDL_WINDOW_FULLSCREEN);
+        
+        assert!(
+            result == 0,
+            "SDL_SetWindowFullscreen failed: {:?}",
+            std::ffi::CStr::from_ptr(SDL_GetError())
+        );
     }
 
     true
@@ -86,9 +110,9 @@ fn setup() {
         .lock()
         .expect("Failed to get the lock of the COLOR_BUFFER");
 
-    *buffer_option = Some(vec![0; (WINDOW_WIDTH * WINDOW_HEIGHT) as usize]);
-
     unsafe {
+        *buffer_option = Some(vec![0; (WINDOW_WIDTH * WINDOW_HEIGHT) as usize]);
+
         // Creating the SDL texture that is used to display the color
         SDL_TEXTURE = SDL_CreateTexture(
             SDL_RENDERER,
@@ -102,6 +126,7 @@ fn setup() {
 
 fn process_input() {
     unsafe {
+        // REMEBER TO USE THIS PATTERN WHEN CREATING A MUTABLE REFERENCE
         let mut event: SDL_Event = std::mem::zeroed();
         SDL_PollEvent(&mut event);
 
@@ -124,11 +149,13 @@ fn clear_color_buffer(color: u32) {
         .lock()
         .expect("Failed to acquire lock for the COLOR_BUFFER");
 
-    if let Some(buffer) = buffer_option.as_mut() {
-        for y in 0..WINDOW_HEIGHT {
-            for x in 0..WINDOW_WIDTH {
-                // Gets the cell (pixel) on the screen by row + col
-                buffer[((WINDOW_WIDTH * y) + x) as usize] = color;
+    unsafe {
+        if let Some(buffer) = buffer_option.as_mut() {
+            for y in 0..WINDOW_HEIGHT {
+                for x in 0..WINDOW_WIDTH {
+                    // Gets the cell (pixel) on the screen by row + col
+                    buffer[((WINDOW_WIDTH * y) + x) as usize] = color;
+                }
             }
         }
     }
