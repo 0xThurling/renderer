a383497926ca22b5f22908a2e7596821cc451782
Jacques Thurling <jthurling78@gmail.com>
2025-09-21 11:20:02 +0200

    .gitignore is now working

--- build/CMakeFiles/renderer.dir/src/triangle.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/renderer.dir/src/triangle.cpp.o: \
-  /Users/jacquesthurling/personal-projects/render/renderer/src/triangle.cpp \
-  /Users/jacquesthurling/personal-projects/render/renderer/src/triangle.h \
-  /Users/jacquesthurling/personal-projects/render/renderer/src/vector.h
