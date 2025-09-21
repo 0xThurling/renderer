386a96ba0c65cc70b11997ef8d9843103ad2486f
Jacques Thurling <jthurling78@gmail.com>
2025-09-21 11:16:53 +0200

    Reworked the project in c++

---
+++ build/CMakeFiles/renderer.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jacquesthurling/personal-projects/render/renderer/src/array.cpp" "CMakeFiles/renderer.dir/src/array.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/array.cpp.o.d"
+  "/Users/jacquesthurling/personal-projects/render/renderer/src/display.cpp" "CMakeFiles/renderer.dir/src/display.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/display.cpp.o.d"
+  "/Users/jacquesthurling/personal-projects/render/renderer/src/main.cpp" "CMakeFiles/renderer.dir/src/main.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/main.cpp.o.d"
+  "/Users/jacquesthurling/personal-projects/render/renderer/src/mesh.cpp" "CMakeFiles/renderer.dir/src/mesh.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/mesh.cpp.o.d"
+  "/Users/jacquesthurling/personal-projects/render/renderer/src/triangle.cpp" "CMakeFiles/renderer.dir/src/triangle.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/triangle.cpp.o.d"
+  "/Users/jacquesthurling/personal-projects/render/renderer/src/vector.cpp" "CMakeFiles/renderer.dir/src/vector.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/vector.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/macros.rs
+++
@@ -1,8 +0,0 @@
-macro_rules! sdl_ticks_passed {
-    ($a: expr, $b: expr) => {
-        // $b is my deadline, $a is my current ticks 
-        (($b).wrapping_sub($a) as i32) <= 0
-    };
-}
-
-
