680567cc976429e6663117bf4a7e234790dc9369
0xChillin <jthurling78@gmail.com>
2025-11-18 18:56:41 +0200

    Added the world matrix to the renderer

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <ostream>
-#include <string>
 
 /////////////////////////////////////////////////////////////
 //// Array of triangle that should be rendered frame by frame
@@ -158,13 +157,15 @@ void update() {
     for (int j = 0; j < 3; j++) {
       vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-      transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);    
-
-      transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);    
-      transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);    
-      transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);    
-
-      transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);    
+      // Create a world matrix by combining scale, rotation and translation matrices
+      mat4_t world_matrix = mat4_identity();
+      world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+      world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+      world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
+      
+      transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
 
       // Save transformed vertex in the array of the transformed vertices
       transformed_vertices[j] = transformed_vertex;
